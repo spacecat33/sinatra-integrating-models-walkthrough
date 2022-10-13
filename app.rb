4612ce0d772cfcd701cced480db5bc0fea006b6d
Janan Akkad <jananakkad@hotmail.co.uk>
2022-10-13 14:07:44 +0100

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(text_from_user)
     erb :results
   end
 end
+
+# for the second method, We can shorten this to: ```ruby post '/' do 
+# @analyzed_text = TextAnalyzer.new(params[:user_text])
+# erb :results end ```
